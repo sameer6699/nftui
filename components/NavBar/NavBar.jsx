a946e705532d9b9fc2302028ee5c2f43a0637c28
Sameer Prashant Jadhav <sammyjadhav6699@gmail.com>
2024-07-10 11:40:48 +0530

    10-7-2024 Commit

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-// ----Importing Icons 
+
+// ----Importing Icons----------------------// 
 import {MdNotifications} from 'react-icons/md';
 import {BsSearch} from "react-icons/bs";
 import {CgMenueLeft, CgMenueRight} from "react-icons/cg";
 
-// Intranal Import 
+//-------- Intranal Import ------------------// 
 import Style from './NavBar.module.css';
 import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index";
 import {Button} from '../componentindex';
@@ -16,14 +17,17 @@ import images from '../../img';
 
 const NavBar = () => {
   //-------- USESTATE COMPONENTS 
-  
   const [discover, setdiscover] = useState(false);
   const [help, sethelp] = useState(false);
-  const [Notification, setnotification] = useState(false); 
+  const [notification, setnotification] = useState(false); 
   const [profile, setprofile] = useState(false);
   const [openSideMenu, setOpenSideMenu] = useState(false);
 
-  return <div>NavBar</div>
+  return (
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}></div>
+    </div>
+  )
 }
 
 export default NavBar
--- components/componentindex.js
+++ components/componentindex.js
@@ -1,10 +1,4 @@
-import Discover from "./NavBar/Discover/Discover";
-import HelpCenter from "./NavBar/HelpCenter/HelpCenter";
-import Notification from "./NavBar/Notification/Notification";
-import Profile from "./NavBar/Profile/Profile";
-import SideBar from "./NavBar/SideBar/SideBar";
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
 
-
-export {Discover, HelpCenter,Notification,Profile, SideBar, Button, Footer}
